706099a129fa016655c3f1797ff7da2bca6ee238
Kevin Gil Rausa Soriano <ksoriano@ad.acquireap>
2018-03-26 12:24:20 +0800

    routes

---
+++ resources/views/Layout/nav.blade.php
@@ -0,0 +1,41 @@
+<header>
+
+    <!-- topmost nav --->
+    <nav class="nav__contact">
+
+
+
+    </nav>
+    <!-- end of top nav --->
+
+    <nav class="nav__menu">
+
+        <div>
+            <img src="">
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>Why Ibinex</li>
+            <li>Pricing</li>
+            <li>Services</li>
+            <li>About Us</li>
+            <li>Security</li>
+            <li>Legal</li>
+            <li>Contact Us</li>
+        </ul>
+
+        <div>
+            <button>Live Demo</button>
+        </div>
+
+    </nav>
+
+    <nav class="nav__currency">
+
+
+
+    </nav>
+
+
+</header>
